6daf77d661bac1e98ed5d9bc5f9336a148d69810
Phil Stevenson <philipstevenson@icloud.com>
2026-02-03 15:10:22 +0000

    test: failing test for validate ticket type requests

--- cinema-tickets-javascript/test/TicketService.test.js
+++ cinema-tickets-javascript/test/TicketService.test.js
@@ -1,23 +1,34 @@
 import TicketService from '../src/pairtest/TicketService.js';
+import TicketTypeRequest from '../src/pairtest/lib/TicketTypeRequest.js';
 
 describe('Test TicketService class', () => {
   const ticketService = new TicketService();
 
-  test('purchaseTickets valid account id (1)', () => {
+  test('purchaseTickets valid account id (1) and single adult ticket', () => {
+    const accountId = 1
+    const ticketTypeRequest = new TicketTypeRequest('ADULT', 1)
+
     expect(
-      ticketService.purchaseTickets(1)
+      ticketService.purchaseTickets(
+        accountId,
+        ticketTypeRequest
+      )
     ).toBe(true);
   })
 
   test('purchaseTickets invalid account id (0)', () => {
+    const accountId = 0
+
     expect(
-      ticketService.purchaseTickets(0)
+      ticketService.purchaseTickets(accountId)
     ).toBe(false);
   })
 
   test('purchaseTickets invalid account id ("foo")', () => {
+    const accountId = "foo"
+
     expect(
-      ticketService.purchaseTickets("foo")
+      ticketService.purchaseTickets(accountId)
     ).toBe(false);
   })
 })
