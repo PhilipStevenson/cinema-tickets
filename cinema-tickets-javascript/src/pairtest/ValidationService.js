3cff96eb08d6d3c965aaa915bf9028358bf1aaad
Phil Stevenson <philipstevenson@icloud.com>
2026-02-04 10:51:59 +0000

    feat: update validation methods to throw error rather than return false

--- cinema-tickets-javascript/src/pairtest/ValidationService.js
+++ cinema-tickets-javascript/src/pairtest/ValidationService.js
@@ -1,31 +1,43 @@
 import TicketTypeRequest from "./lib/TicketTypeRequest";
 
 export default class ValidationService {
-
   // Validate account id is of type int and more than zero.
   validateAccountId(accountId) {
     if (Number.isInteger(accountId) && accountId > 0) {
       return true;
     } else {
-      return false;
+      throw new TypeError("accountId must be an integer and more than 0");
     }
   }
 
   // Validate ticket type request is instance of TicketTypeRequest class.
   validateTicketTypeRequestType(ticketTypeRequests) {
-    return ticketTypeRequests.every(
-      (request) => request instanceof TicketTypeRequest
-    ) ? true : false;
+    if (
+      ticketTypeRequests.every(
+        (request) => request instanceof TicketTypeRequest,
+      )
+    ) {
+      return true;
+    } else {
+      throw new TypeError(
+        "ticketTypeRequests must consist of type TicketTypeRequest",
+      );
+    }
   }
 
   // Validate total number of tickets requested is 25 max.
   validateNumberOfTicketsRequested(ticketTypeRequests, maxTickets) {
     const initialValue = 0;
     const totalNumberOfTicketsRequested = ticketTypeRequests.reduce(
-      (accumulator, currentValue) => accumulator + currentValue.getNoOfTickets(),
-      initialValue
+      (accumulator, currentValue) =>
+        accumulator + currentValue.getNoOfTickets(),
+      initialValue,
     );
-    
-    return totalNumberOfTicketsRequested <= maxTickets ? true : false;
+
+    if (totalNumberOfTicketsRequested <= maxTickets) {
+      return true;
+    } else {
+      throw new Error(`number of requested tickets exceeds ${maxTickets}`);
+    }
   }
 }
