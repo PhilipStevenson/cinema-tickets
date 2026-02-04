7b811eaaebc6db4aebf5b6d6e9461293d42cb5bb
Phil Stevenson <philipstevenson@icloud.com>
2026-02-04 11:37:52 +0000

    feat: private methods to get number of seats and cost

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,4 +1,4 @@
-import ValidationService from './ValidationService.js';
+import ValidationService from "./ValidationService.js";
 
 export default class TicketService {
   /**
@@ -6,21 +6,56 @@ export default class TicketService {
    */
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
+    console.log(accountId);
+    console.log(ticketTypeRequests);
 
-    console.log(accountId)
-    console.log(ticketTypeRequests)
-
-    const validationService = new ValidationService
+    const validationService = new ValidationService();
 
     if (
       validationService.validateAccountId(accountId) == true &&
-      validationService.validateTicketTypeRequestType(ticketTypeRequests) == true &&
-      validationService.validateNumberOfTicketsRequested(ticketTypeRequests, 25) == true
+      validationService.validateTicketTypeRequestType(ticketTypeRequests) ==
+        true &&
+      validationService.validateNumberOfTicketsRequested(
+        ticketTypeRequests,
+        25,
+      ) == true
     ) {
       return true;
     } else {
       return false;
       // throw InvalidPurchaseException
     }
+  }
+
+  // Get number of chargeable seats.
+  #getNumberOfChargeableSeats(ticketTypeRequests) {
+    const initialValue = 0;
+    const totalNumberOfChargeableSeats = ticketTypeRequests.reduce(
+      (accumulator, currentValue) => {
+        if (currentValue.getNoOfTickets() != "INFANT") {
+          accumulator + currentValue.getNoOfTickets();
+        }
+      },
+      initialValue,
+    );
+
+    return totalNumberOfChargeableSeats;
+  }
+
+  // Get total cost of chargeable seats.
+  #getTotalCostOfChargeableSeats(ticketTypeRequests) {
+    const initialValue = 0;
+    const totalCostOfChargeableSeats = ticketTypeRequests.reduce(
+      (accumulator, currentValue) => {
+        if (currentValue.getTicketType() == "CHILD") {
+          accumulator + currentValue.getNoOfTickets() * 15;
+        } else if (currentValue.getTicketType() == "ADULT") {
+          accumulator + currentValue.getNoOfTickets() * 25;
+        }
+      },
+      initialValue,
+    );
+
+    return totalCostOfChargeableSeats;
   }
 }
